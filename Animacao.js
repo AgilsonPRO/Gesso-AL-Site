45e885996254e702593a8613e1eb5194a90866c2
Agilson Alves <agilson.profissional@gmail.com>
2022-03-07 12:14:50 -0300

    Adicionado função para esconder/ mostrar area de "atendimento" no index.html

--- Animacao.js
+++ Animacao.js
@@ -1,7 +1,9 @@
 class Animacao {
     constructor() {
+       this.posicaoAtend     = document.getElementById('calculator') //monitora a posição do elemento
        this.posicaoVantagens = document.getElementById('Vantagens');
        this.posicaoServicos  = document.getElementById ('tools');
+       this.Atende           = document.querySelector('.Atendimento'); //seleciona elemento a ser animado
        this.cardvant         = document.querySelector('.card-vant');
        this.cardserv         = document.querySelector('.card-serv');   
     }
@@ -9,6 +11,7 @@ class Animacao {
     scrollCard() {
         window.requestAnimationFrame (this.CalculoScroll.bind(this));
         window.requestAnimationFrame (this.CalculoScroll1.bind(this));
+        window.requestAnimationFrame (this.CalculoScroll2.bind(this));
     }
     //Cards aparecendo da esquerda para direita de acordo com a posição da tela
     CalculoScroll() {
@@ -27,9 +30,16 @@ class Animacao {
         this.cardserv.style.transform =`translate(${((posicao )- 108)/10}%)`;
         }
     }
+    CalculoScroll2(){
+        const posicao = this.posicaoAtend.getBoundingClientRect()['y']
+        console.log(posicao);
+        if (posicao <= 111){
+        this.Atende.style = "visibility: visible;";
+        }else{
+        this.Atende.style = "visibility: hidden;";    
+        }
+    }
     
-
-
 }
 
 export { Animacao }
